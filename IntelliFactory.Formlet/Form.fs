d224b6b257fcf3bc789cb30a33ba849946a52863
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2011-12-07 16:59:36 -0500

    Fixed licensing and whitespace.

--- IntelliFactory.Formlet/Form.fs
+++ IntelliFactory.Formlet/Form.fs
@@ -1,10 +1,22 @@
-// This file is confidential and proprietary.
+// $begin{copyright}
+// 
+// This file is part of WebSharper
+// 
+// Copyright (c) 2008-2011 IntelliFactory
+// 
+// GNU Affero General Public License Usage
+// WebSharper is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License, version 3, as published
+// by the Free Software Foundation.
 //
-// Copyright (c) IntelliFactory, 2004-2011.
+// WebSharper is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+// for more details at <http://www.gnu.org/licenses/>.
 //
-// All rights reserved.  Reproduction or use in whole or in part is
-// prohibited without the written consent of the copyright holder.
-//-----------------------------------------------------------------
+// If you are unsure which license is appropriate for your use, please contact
+// IntelliFactory at http://intellifactory.com/contact.
+//
 // $end{copyright}
 
 namespace IntelliFactory.Formlet.Base
