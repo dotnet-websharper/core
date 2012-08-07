40187e0a58346c1e8588d74f5af47752e101f66d
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2012-08-07 13:31:44 -0400

    Fixed #77

--- IntelliFactory.WebSharper.Sitelets/Offline/Extra.fs
+++ IntelliFactory.WebSharper.Sitelets/Offline/Extra.fs
@@ -21,80 +21,243 @@
 
 module IntelliFactory.WebSharper.Sitelets.Offline.Extra
 
+open System
+open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 
-// the type of an extra, either a File or an entire Directory, to be copied
-type T =
-    | File of string
-    | Directory of string
+let invalidPathChars =
+    HashSet(Path.GetInvalidPathChars())
 
-let ( ++ ) a b = Path.Combine(a, b)
+let isValidPath (path: string) =
+    path <> null && not (Seq.exists invalidPathChars.Contains path)
 
-// naively copies a directory. fine for our purposes.
-let rec CopyDirectory src dest =
-    if Directory.Exists dest |> not then
-        Directory.CreateDirectory dest |> ignore
+let invalidFilePatternChars =
+    let h = HashSet(Path.GetInvalidFileNameChars())
+    h.Remove('*') |> ignore
+    h.Remove('?') |> ignore
+    h
 
-    for s in Directory.GetFiles src do
-        let d = Path.Combine(dest, Path.GetFileName s)
-        File.Copy(s, d, true)
+let isValidFilePattern (filename: string) =
+    if filename = null then
+        nullArg "filename in isValidFileName"
+    match filename.ToUpper() with
+    | "CON" | "PRN" | "AUX" | "NUL"
+    | "COM1" | "COM2" | "COM3" | "COM4" | "COM5"
+    | "COM6" | "COM7" | "COM8" | "COM9"
+    | "LPT1" | "LPT2" | "LPT3" | "LPT4"
+    | "LPT5" | "LPT6" | "LPT7" | "LPT8" | "LPT9" -> false
+    | _ -> not (Seq.exists invalidFilePatternChars.Contains filename)
 
-    for dir in Directory.GetDirectories src do
-        let d = Path.Combine(dest, Path.GetFileName dir)
-        CopyDirectory dir d
+type Result<'T> =
+    | Ok of 'T
+    | Error of string
 
-// copies a t
-let copy dest t =
-    match t with
-    | File s ->  File.Copy(s, dest, true)
-    | Directory s -> CopyDirectory s dest
+    member this.Value =
+        match this with
+        | Ok x -> x
+        | Error e -> failwith e
 
-// gets the name of a t
-let GetName t =
-    match t with
-    | File s | Directory s -> s
+type LocalPath =
+    | RPath
+    | SPath of LocalPath * string
 
-// parses extra.config into a list of paths
-// format
-// <project-name> {
-// <file-pattern>*
-// }
-// ...
-// example
-// WebProject {
-//   css/*
-//   images/*
-// }
-// param path : the directory containing extra.files
-// param text : the text of the file
-let parse (path: string) (text: string) : List<T> =
-    let mutable l = []
-    let matches = Regex.Matches(text, "[^\s]+")
-    for m in matches do
-        let s = path ++ m.Value
-        let t = 
-            if (s.Chars (s.Length - 1)) = '*' then
-                Directory (s.Substring(0, s.Length - 1))
-            else
-            File s
-        l <- t :: l
-    l
+    member this.SubPath(subPath: string) =
+        if isValidFilePattern subPath then
+            match subPath, this with
+            | ".", _
+            | "..", RPath ->  Ok this
+            | "..", SPath (x, _) -> Ok x
+            | _ -> Ok (SPath (this, subPath))
+        else
+            Error ("Invalid filename pattern: " + string subPath)
 
-// our entry point
-// dir is the directory of extra.files
-let CopyFiles dir dest = 
-    printf "%s\n" dir
-    let extras = dir ++ "extra.files"
-    if File.Exists extras then
-        printf "Copying files specified in %s\n" extras
-        File.ReadAllLines extras
-        |> Array.filter (fun l -> l.TrimStart([| '\t'; ' ' |]).IndexOf("//") <> 0) // filter out comments first
-        |> String.concat "\n" // put the rest back together
-        |> parse dir
-        |> List.iter (fun x ->
-            GetName x
-            |> printf "Copying %s\n"
-            copy (dest ++ Path.GetFileName (GetName x)) x)
+    override this.ToString() =
+        use w = new StringWriter()
+        this.Write(w)
+        w.ToString()
+
+    member this.Write(w: TextWriter) =
+        match this with
+        | RPath -> w.Write('.')
+        | SPath (p, x) ->
+            p.Write(w)
+            w.Write(Path.DirectorySeparatorChar)
+            w.Write(x)
+
+    static member Parse(path: string) =
+        if isValidPath path then
+            let chars =
+                [|
+                    Path.AltDirectorySeparatorChar
+                    Path.DirectorySeparatorChar
+                |]
+            path.Split(chars, StringSplitOptions.RemoveEmptyEntries)
+            |> Seq.fold (fun root subPath ->
+                match root with
+                | Ok root ->
+                    if isValidFilePattern subPath then
+                        match subPath with
+                        | "." -> Ok root
+                        | ".." ->
+                            match root with
+                            | SPath (root, _) -> Ok root
+                            | RPath -> Ok RPath
+                        | _ -> Ok (root / subPath)
+                    else
+                        Error ("Invalid filename pattern: " + subPath)
+                | e -> e)
+                (Ok RPath)
+        else
+            Error ("Invalid file path: " + string path)
+
+    static member Root = RPath
+
+    static member ( / ) (self: LocalPath, subPath: string) =
+        self.SubPath(subPath).Value
+
+type EntryKind =
+    | DirectoryKind
+    | FileKind
+
+type Entry =
+    | DirectoryEntry of LocalPath
+    | FileEntry of LocalPath
+
+    member this.Path =
+        match this with
+        | DirectoryEntry p
+        | FileEntry p -> p
+
+let toAbsolutePath basePath localPath =
+    match localPath with
+    | RPath -> basePath
+    | _ -> Path.Combine(basePath, string localPath)
+
+let findEntries (basePath: string) (pattern: LocalPath) =
+
+    let toAbsolutePath =
+        toAbsolutePath basePath
+
+    let getEntry path =
+        let p = toAbsolutePath path
+        if Directory.Exists(p) then
+            Some (DirectoryEntry path)
+        elif File.Exists(p) then
+            Some (FileEntry path)
+        else
+            None
+
+    let findFileChildren p pat =
+        match pat with
+        | None -> Directory.GetFiles(toAbsolutePath p)
+        | Some pat -> Directory.GetFiles(toAbsolutePath p, pat)
+        |> Seq.map (fun x ->
+            FileEntry (p / Path.GetFileName(x)))
+
+    let rec findAllEntries path =
+        match getEntry path with
+        | Some (DirectoryEntry _ as d) ->
+            let ap = toAbsolutePath path
+            seq {
+                yield d
+                yield! findFileChildren path None
+                for d in Directory.GetDirectories(ap) do
+                    yield! findAllEntries (path / Path.GetFileName(d))
+            }
+        | Some e -> Seq.singleton e
+        | None -> Seq.empty
+
+    let rec findFileSystemEntries pattern =
+        match pattern with
+        | RPath ->
+            Option.toArray (getEntry RPath) :> seq<_>
+        | SPath (p1, "**") ->
+            findDirectories p1
+            |> Seq.collect findAllEntries
+        | SPath (p1, p2) ->
+            findDirectories p1
+            |> Seq.collect (fun dir ->
+                let p = toAbsolutePath dir
+                let dirs =
+                    Directory.GetDirectories(p, p2)
+                    |> Seq.map (fun n ->
+                        DirectoryEntry (dir / Path.GetFileName(n)))
+                let files = findFileChildren dir (Some p2)
+                Seq.append dirs files)
+
+    and findDirectories pattern =
+        findFileSystemEntries pattern
+        |> Seq.choose (fun e ->
+            match e with
+            | DirectoryEntry d -> Some d
+            | _ -> None)
+
+    findFileSystemEntries pattern
+    |> Seq.filter (fun e ->
+        match e with
+        | DirectoryEntry RPath -> false
+        | _ -> true)
+    |> Seq.distinct
+    |> Seq.cache
+
+let makeDir (path: string) =
+    if Directory.Exists(path) |> not then
+        Directory.CreateDirectory(path) |> ignore
+        stdout.WriteLine("Created directory: {0}", path)
+
+let copyFile (source: string) (dest: string) =
+    let destDir = Path.GetDirectoryName(dest)
+    makeDir destDir
+    let doCopy () =
+        File.Copy(source, dest, true)
+        stdout.WriteLine("Copied {0} to {1}", source, dest)
+    let getLastWriteTime p =
+        FileInfo(p).LastWriteTimeUtc
+    if File.Exists(dest) then
+        if getLastWriteTime dest < getLastWriteTime source then
+            doCopy ()
     else
-        printf "No extra.files specified. Skipping...\n"
+        doCopy ()
+
+let rec copyDirectory (src: string) (dest: string) =
+    for s in Directory.GetFiles(src) do
+        let d = Path.Combine(dest, Path.GetFileName(s))
+        copyFile s d
+    for dir in Directory.GetDirectories(src) do
+        let d = Path.Combine(dest, Path.GetFileName(dir))
+        copyDirectory dir d
+
+let copyEntryTo (src: string) (dest: string) (entry: Entry) =
+    let p1 = toAbsolutePath src entry.Path
+    let p2 = toAbsolutePath dest entry.Path
+    match entry with
+    | FileEntry _ -> copyFile p1 p2
+    | DirectoryEntry _ -> copyDirectory p1 p2
+
+let commentRegex =
+    Regex(@"\s*//.*")
+
+let removeComments (text: string) =
+    commentRegex.Replace(text, "")
+
+let readLines file =
+    File.ReadAllLines(file)
+    |> Array.choose (fun line ->
+        match removeComments(line).Trim() with
+        | "" -> None
+        | s -> Some s)
+
+let CopyFiles (dir: string) (dest: string) =
+    stdout.WriteLine(dir)
+    let extras = Path.Combine(dir, "extra.files")
+    if File.Exists(extras) then
+        for line in readLines extras do
+            match LocalPath.Parse(line) with
+            | Ok p ->
+                findEntries dir p
+                |> Seq.iter (copyEntryTo dir dest)
+            | Error e ->
+                stderr.WriteLine(e)
+    else
+        stdout.WriteLine("No extra.files specified. Skipping...")
