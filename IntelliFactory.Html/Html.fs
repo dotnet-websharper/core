2eb8ea339fbd55fbdbbfd660e345c7b1a5b2c8a9
Anton Tayanovskyy <name.surname@gmail.com>
2012-09-25 16:11:16 -0400

    Formatting corrections in Html.fs

--- IntelliFactory.Html/Html.fs
+++ IntelliFactory.Html/Html.fs
@@ -20,29 +20,32 @@
 // $end{copyright}
 namespace IntelliFactory.Html
 
+open System
+open System.Collections.Generic
+open System.IO
+open System.Web.UI
+open System.Xml
+
 /// Provides types for expressing HTML values.
 [<AutoOpen>]
 module Html =
-    open System.Collections.Generic
-    open System.IO
-    open System.Web.UI
-    open System.Xml
 
     /// Represents HTML tags.
     type TagContent<'T> =
         {
-            Name        : string
-            Attributes  : list<Attribute<'T>>
-            Contents    : list<Element<'T>>
-            Annotation  : option<'T>
+            Name : string
+            Attributes : list<Attribute<'T>>
+            Contents : list<Element<'T>>
+            Annotation : option<'T>
         }
 
     /// Represents HTML attributes.
     and Attribute<'T> =
         {
-            Name    : string
-            Value   : string
+            Name : string
+            Value : string
         }
+
         interface INode<'T> with
             member this.Node = AttributeNode this
 
@@ -56,15 +59,15 @@ module Html =
 
     /// Represents nodes.
     and Node<'T> =
-        | AttributeNode  of Attribute<'T>
-        | ContentNode    of Element<'T>
+        | AttributeNode of Attribute<'T>
+        | ContentNode of Element<'T>
 
     /// Represents HTML/XML contents.
     and  Element<'T> =
-        | TagContent      of TagContent<'T>
-        | TextContent     of string
+        | TagContent of TagContent<'T>
+        | TextContent of string
         | VerbatimContent of string
-        | CommentContent  of string
+        | CommentContent of string
 
         interface IElement<'T> with
             member this.Element = this
@@ -81,13 +84,12 @@ module Html =
                     |> List.map (fun content -> content.CollectAnnotations())
                     |> List.concat
                 match content.Annotation with
-                | Some a    -> [a]
-                | None      -> []
+                | Some a -> [a]
+                | None -> []
                 |> List.append nested
             | TextContent _
             | VerbatimContent _
-            | CommentContent _ ->
-                []
+            | CommentContent _ -> []
 
     /// Constructs a new HTML element.
     let NewElement (name: string) (elements: seq<#INode<'T>>) =
@@ -95,24 +97,24 @@ module Html =
             elements
             |> Seq.choose (fun x ->
                 match x.Node with
-                | AttributeNode x   -> Some x
-                | _                 -> None)
-            |> Seq.toList   
+                | AttributeNode x -> Some x
+                | _ -> None)
+            |> Seq.toList
 
-        let selCnt (x : INode<_>) =
+        let selCnt (x: INode<_>) =
             match x.Node with
             | ContentNode x -> Some x
-            | _             -> None    
-             
+            | _ -> None
+
         let contents =
             elements
             |> Seq.choose selCnt
             |> Seq.toList
         {
-            Name        = name
-            Attributes  = attrs
-            Contents    = contents
-            Annotation  = None
+            Name = name
+            Attributes = attrs
+            Contents = contents
+            Annotation = None
         }
         |> TagContent
 
@@ -123,20 +125,20 @@ module Html =
                 elements
                 |> Seq.choose (fun x ->
                     match x.Node with
-                    | AttributeNode x   -> Some x
-                    | _                 -> None)
+                    | AttributeNode x -> Some x
+                    | _ -> None)
                 |> Seq.toList
             let newContent =
                 elements
                 |> Seq.choose (fun x ->
                     match x.Node with
                     | ContentNode x -> Some x
-                    | _             -> None)
+                    | _ -> None)
                 |> Seq.toList
             {
-                Name        = el.Name
-                Attributes  = el.Attributes @ newAttrs
-                Contents    = el.Contents @ newContent
+                Name = el.Name
+                Attributes = el.Attributes @ newAttrs
+                Contents = el.Contents @ newContent
                 Annotation = el.Annotation
             }
             |> TagContent
@@ -161,8 +163,7 @@ module Html =
             |> TagContent
         | VerbatimContent _
         | TextContent _
-        | CommentContent _ ->
-            e
+        | CommentContent _ -> e
 
     /// Represents HTML element identifiers.
     type Id = string
@@ -171,36 +172,35 @@ module Html =
     type Document<'T> =
         {
             Annotations : Map<Id,'T>
-            Body        : Element<unit>
+            Body : Element<unit>
         }
 
     /// Resolves the annotations by giving them HTML identifiers. The function
     /// `isUsed` tests if a given identifier should not be used by the document.
     let Resolve (isUsed: Id -> bool) (document: TagContent<'T>) =
-        let ids         = Dictionary<Id,unit>()
+        let ids = Dictionary<Id,unit>()
         let annotations = Dictionary<Id,'T>()
-        let showId (k: int) =
-            System.String.Format("id{0:x}", k)
+        let showId (k: int) = String.Format("id{0:x}", k)
         let newId () =
             let mutable k  = ids.Count
             let mutable id = showId k
             while ids.ContainsKey id || isUsed id do
-                k   <- k + 1
-                id  <- showId k
+                k <- k + 1
+                id <- showId k
             ids.[id] <- ()
             id
         let rec transform (element: TagContent<'T>) =
-            let mutable attributes  = []
-            let mutable id          = None
+            let mutable attributes = []
+            let mutable id = None
             for x in element.Attributes do
                 match x.Name.ToLower() with
-                | "id"  -> ids.[x.Value] <- (); id <- Some x.Value
-                | _     -> ()
+                | "id" -> ids.[x.Value] <- (); id <- Some x.Value
+                | _ -> ()
                 let attribute = { Name = x.Name; Value = x.Value }
                 attributes <- attribute :: attributes
             match element.Annotation with
-            | None      -> ()
-            | Some a    ->
+            | None -> ()
+            | Some a ->
                 let id =
                     match id with
                     | Some id   -> id
@@ -221,14 +221,14 @@ module Html =
                 | TextContent e ->
                     contents <- TextContent e :: contents
             {
-                Name        = element.Name // was: document.Name??
-                Attributes  = List.rev attributes
-                Contents    = List.rev contents
-                Annotation  = None
+                Name = element.Name // was: document.Name??
+                Attributes = List.rev attributes
+                Contents = List.rev contents
+                Annotation = None
             }
             |> TagContent
         {
-            Body        = transform document
+            Body = transform document
             Annotations =
                 seq { for kv in annotations -> (kv.Key, kv.Value) }
                 |> Map.ofSeq
@@ -251,8 +251,8 @@ module Html =
             "PARAM"
         ]
         |> List.exists (fun x -> x.ToLower() = name.ToLower())
-        
-    
+
+
     /// Writes HTML nodes to an XmlWriter.
     [<Sealed>]
     type Writer (writer: HtmlTextWriter) =
@@ -260,7 +260,7 @@ module Html =
         new (writer: TextWriter) =
             new Writer (new HtmlTextWriter(writer))
 
-        // Renders an element without content and with self 
+        // Renders an element without content and with self
         // closing tagE.g. <img class="c" />
         member private this.RenderElementWithSelfClosingTag name (attrs: list<Attribute<'T>>) =
             writer.WriteBeginTag(name)
@@ -268,16 +268,17 @@ module Html =
                 writer.WriteAttribute(a.Name, a.Value)
             writer.Write(HtmlTextWriter.SelfClosingTagEnd)
 
-        // Renders tag with content. 
+        // Renders tag with content.
         // E.g. <div class="c">...</div>
-        member private this.RenderElementWithContent    name
-                                            (attrs: list<Attribute<'T>>) 
-                                            (contents : list<Element<'T>>) =
+        member private this.RenderElementWithContent name
+                (attrs: list<Attribute<'T>>)
+                (contents : list<Element<'T>>) =
+
             writer.WriteBeginTag(name)
             for a in attrs do
                 writer.WriteAttribute(a.Name, a.Value)
             writer.Write(HtmlTextWriter.TagRightChar)
-        
+
             // Render contents
             for content in contents do
                 this.Write content
@@ -292,16 +293,6 @@ module Html =
                     this.RenderElementWithSelfClosingTag element.Name element.Attributes
                 | contents ->
                     this.RenderElementWithContent element.Name element.Attributes contents
-
-//                for a in element.Attributes do
-//                    writer.AddAttribute(a.Name, a.Value, true)
-//                writer.RenderBeginTag element.Name
-//                let mutable isEmpty = true
-//                for content in element.Contents do
-//                    this.Write content
-//                    isEmpty <- false
-//                writer.RenderEndTag()
-                
             | TextContent text ->
                 writer.WriteEncodedText text
             | VerbatimContent text ->
@@ -319,6 +310,6 @@ module Html =
         /// Returns the inner XmlWriter.
         member this.Writer = writer
 
-        interface System.IDisposable with
+        interface IDisposable with
             member this.Dispose() =
-                (writer :> System.IDisposable).Dispose()
+                (writer :> IDisposable).Dispose()
