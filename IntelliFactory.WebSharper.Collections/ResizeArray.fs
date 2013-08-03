83ddae7d935628a080cbcce96816eebb7b4edb8f
Loic Denuziere <loic.denuziere@intellifactory.com>
2013-08-03 18:38:55 +0200

    Implement ResizeArray.GetEnumerator and Item setter.
    --HG--
    branch : security

--- IntelliFactory.WebSharper.Collections/ResizeArray.fs
+++ IntelliFactory.WebSharper.Collections/ResizeArray.fs
@@ -45,6 +45,10 @@ type ResizeArrayProxy<'T> [<JavaScript>] (arr: 'T []) =
         new ResizeArrayProxy<'T>(Seq.toArray el)
 
     [<JavaScript>]
+    member this.GetEnumerator() =
+        (As<seq<obj>> arr).GetEnumerator()
+
+    [<JavaScript>]
     member this.Add(x: 'T) : unit =
         push arr x
 
@@ -83,8 +87,9 @@ type ResizeArrayProxy<'T> [<JavaScript>] (arr: 'T []) =
     member this.InsertRange(index: int, items: seq<'T>) : unit =
         splice arr index 0 (Seq.toArray items) |> ignore
 
-    [<JavaScript>]
-    member this.Item with get (x: int) : 'T = arr.[x]
+    member this.Item
+        with [<JavaScript>] get (x: int) : 'T = arr.[x]
+        and [<JavaScript>] set (x: int) (v: 'T) = arr.[x] <- v
 
     [<JavaScript>]
     member this.RemoveAt(x: int) : unit =
