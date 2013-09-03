085cf7db18769a1f48091e5d83b1a65e8cdab1c8
jankoa <none@none>
2013-09-03 18:16:50 +0200

    Array module functions are using ES5 functions when appropriate
    --HG--
    extra : source : 61b4bf21f18a3fc6a3f5f042daa2987ec43f7a95

--- IntelliFactory.JavaScript/Runtime.js
+++ IntelliFactory.JavaScript/Runtime.js
@@ -178,3 +178,184 @@ var IntelliFactory =
             }
     }
 };
+
+// EcmaScript 5 fallbacks
+
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function (fn, scope) {
+        'use strict';
+        var i, len;
+        for (i = 0, len = this.length; i < len; ++i) {
+            if (i in this) {
+                fn.call(scope, this[i], i, this);
+            }
+        }
+    };
+}
+
+if (!Array.prototype.every) {
+    Array.prototype.every = function (fun /*, thisp */) {
+        'use strict';
+        var t, len, i, thisp;
+        if (this == null) {
+            throw new TypeError();
+        }
+        t = Object(this);
+        len = t.length >>> 0;
+        if (typeof fun !== 'function') {
+            throw new TypeError();
+        }
+        thisp = arguments[1];
+        for (i = 0; i < len; i++) {
+            if (i in t && !fun.call(thisp, t[i], i, t)) {
+                return false;
+            }
+        }
+        return true;
+    };
+}
+
+if (!Array.prototype.some) {
+    Array.prototype.some = function (fun /*, thisp */) {
+        'use strict';
+        if (this == null) {
+            throw new TypeError();
+        }
+        var thisp, i,
+            t = Object(this),
+            len = t.length >>> 0;
+        if (typeof fun !== 'function') {
+            throw new TypeError();
+        }
+        thisp = arguments[1];
+        for (i = 0; i < len; i++) {
+            if (i in t && fun.call(thisp, t[i], i, t)) {
+                return true;
+            }
+        }
+        return false;
+    };
+}
+
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function (fun /*, thisp*/) {
+        'use strict';
+        if (!this) {
+            throw new TypeError();
+        }
+        var objects = Object(this);
+        var len = objects.length >>> 0;
+        if (typeof fun !== 'function') {
+            throw new TypeError();
+        }
+        var res = [];
+        var thisp = arguments[1];
+        for (var i in objects) {
+            if (objects.hasOwnProperty(i)) {
+                if (fun.call(thisp, objects[i], i, objects)) {
+                    res.push(objects[i]);
+                }
+            }
+        }
+        return res;
+    };
+}
+
+if (!Array.prototype.map) {
+    Array.prototype.map = function (callback, thisArg) {
+        var T, A, k;
+        if (this == null) {
+            throw new TypeError(" this is null or not defined");
+        }
+        var O = Object(this);
+        var len = O.length >>> 0;
+        if (typeof callback !== "function") {
+            throw new TypeError(callback + " is not a function");
+        }
+        if (thisArg) {
+            T = thisArg;
+        }
+        A = new Array(len);
+        k = 0;
+        while (k < len) {
+            var kValue, mappedValue;
+            if (k in O) {
+                kValue = O[k];
+                mappedValue = callback.call(T, kValue, k, O);
+                A[k] = mappedValue;
+            }
+            k++;
+        }
+        return A;
+    };
+}
+
+if ('function' !== typeof Array.prototype.reduce) {
+    Array.prototype.reduce = function (callback, opt_initialValue) {
+        'use strict';
+        if (null === this || 'undefined' === typeof this) {
+            throw new TypeError(
+                'Array.prototype.reduce called on null or undefined');
+        }
+        if ('function' !== typeof callback) {
+            throw new TypeError(callback + ' is not a function');
+        }
+        var index, value,
+            length = this.length >>> 0,
+            isValueSet = false;
+        if (1 < arguments.length) {
+            value = opt_initialValue;
+            isValueSet = true;
+        }
+        for (index = 0; length > index; ++index) {
+            if (this.hasOwnProperty(index)) {
+                if (isValueSet) {
+                    value = callback(value, this[index], index, this);
+                }
+                else {
+                    value = this[index];
+                    isValueSet = true;
+                }
+            }
+        }
+        if (!isValueSet) {
+            throw new TypeError('Reduce of empty array with no initial value');
+        }
+        return value;
+    };
+}
+
+if ('function' !== typeof Array.prototype.reduceRight) {
+    Array.prototype.reduceRight = function (callback, opt_initialValue) {
+        'use strict';
+        if (null === this || 'undefined' === typeof this) {
+            throw new TypeError(
+                'Array.prototype.reduceRight called on null or undefined');
+        }
+        if ('function' !== typeof callback) {
+            throw new TypeError(callback + ' is not a function');
+        }
+        var index, value,
+            length = this.length >>> 0,
+            isValueSet = false;
+        if (1 < arguments.length) {
+            value = opt_initialValue;
+            isValueSet = true;
+        }
+        for (index = length - 1; -1 < index; --index) {
+            if (!this.hasOwnProperty(index)) {
+                if (isValueSet) {
+                    value = callback(value, this[index], index, this);
+                }
+                else {
+                    value = this[index];
+                    isValueSet = true;
+                }
+            }
+        }
+        if (!isValueSet) {
+            throw new TypeError('Reduce of empty array with no initial value');
+        }
+        return value;
+    };
+}
