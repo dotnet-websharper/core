fe85f5f46b316f368139650c24ba3b69c8c2db09
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2013-09-12 10:25:05 -0400

    Set vn in Build.cmd
    --HG--
    branch : security

--- Build.cmd
+++ Build.cmd
@@ -1,9 +1,8 @@
 @ECHO OFF
-REM NOTE: This file was auto-generated with `IB.exe prepare` from `IntelliFactory.Build`.
 IF NOT "%NuGetHome%"=="" GOTO :nuget
 SET NuGetHome=tools\NuGet
 :nuget
-"%NuGetHome%\NuGet.exe" install IntelliFactory.Build -pre -ExcludeVersion -o tools\packages
+"%NuGetHome%\NuGet.exe" install IntelliFactory.Build -version 0.2.24-alpha -pre -ExcludeVersion -o tools\packages
 IF NOT "%FSharpHome%"=="" GOTO :fs
 SET PF=%ProgramFiles(x86)%
 IF NOT "%PF%"=="" GOTO w64
