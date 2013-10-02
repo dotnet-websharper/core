227c9088e2a8cee7475179c6439f7abca2ffadba
"Anton Tayanovskyy (t0yv0) ext:(%22) <anton.tayanovskyy@gmail.com>
2013-10-01 17:28:42 -0700

    Fix #13; Fix #161; Fix #162

--- Build.cmd
+++ Build.cmd
@@ -2,7 +2,7 @@
 IF NOT "%NuGetHome%"=="" GOTO :nuget
 SET NuGetHome=tools\NuGet
 :nuget
-"%NuGetHome%\NuGet.exe" install IntelliFactory.Build -version 0.2.24-alpha -pre -ExcludeVersion -o tools\packages
+"%NuGetHome%\NuGet.exe" install IntelliFactory.Build -pre -ExcludeVersion -o tools\packages
 IF NOT "%FSharpHome%"=="" GOTO :fs
 SET PF=%ProgramFiles(x86)%
 IF NOT "%PF%"=="" GOTO w64
