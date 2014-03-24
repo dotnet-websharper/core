04ed206c8211d7d69e844155849e11a0731bb6be
"Anton Tayanovskyy (t0yv0) ext:(%22) <anton.tayanovskyy@gmail.com>
2014-03-24 13:46:39 -0400

    Build fixes.
    --HG--
    branch : msxbuild

---
+++ tools/configure.fsx
@@ -0,0 +1,29 @@
+open System
+open System.Diagnostics
+open System.IO
+
+let exec (fileName: string) (arguments: string) =
+    let psi = ProcessStartInfo()
+    psi.FileName <- fileName
+    psi.Arguments <- arguments
+    psi.CreateNoWindow <- true
+    psi.UseShellExecute <- false
+    psi.RedirectStandardError <- true
+    psi.RedirectStandardOutput <- true
+    let proc = Process.Start(psi)
+    proc.WaitForExit()
+    proc.StandardError.ReadToEnd() |> stderr.Write
+    proc.StandardOutput.ReadToEnd() |> stdout.Write
+    proc.ExitCode = 0
+
+let loc path =
+    Path.Combine(__SOURCE_DIRECTORY__, path)
+
+let nuget cmd =
+    exec (loc "NuGet/NuGet.exe") cmd
+
+let ok =
+    nuget "install IntelliFactory.Core -pre -o packages -excludeVersion"
+    && nuget "install Mono.Cecil -o packages -excludeVersion"
+    && nuget "install AjaxMin -o packages -excludeVersion"
+
