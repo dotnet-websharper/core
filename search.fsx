2d70785276aed0dfd896ae4e6c6c8183e03503fa
"Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-04-01 10:03:13 -0400

    Remove unused file.
    --HG--
    branch : msxbuild

--- search.fsx
+++
@@ -1,6 +0,0 @@
-open System
-open System.IO
-
-for proj in Directory.EnumerateFiles(__SOURCE_DIRECTORY__, "*.fsproj", SearchOption.AllDirectories) do
-    if File.ReadAllText(proj).Contains("PathConventions") then
-        printfn "%s" proj
