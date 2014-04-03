afe0a1b00eee011efe393bf74224982e8c49d40a
jankoa <none@none>
2014-04-03 15:41:15 +0200

    added support for 2-dim arrays and F#3.1 slicing operators

--- IntelliFactory.WebSharper/Proxy/Array2DModule.fs
+++ IntelliFactory.WebSharper/Proxy/Array2DModule.fs
@@ -1,4 +1,4 @@
-// $begin{copyright}
+﻿// $begin{copyright}
 //
 // This file is part of WebSharper
 //
@@ -19,19 +19,35 @@
 //
 // $end{copyright}
 
+[<IntelliFactory.WebSharper.Core.Attributes.Name "Arrays2D">]
 [<IntelliFactory.WebSharper.Core.Attributes.Proxy
-    "Microsoft.FSharp.Core.ExtraTopLevelOperators, \
+    "Microsoft.FSharp.Collections.Array2DModule, \
      FSharp.Core, Culture=neutral, \
      PublicKeyToken=b03f5f7f11d50a3a">]
-module private IntelliFactory.WebSharper.ExtraTopLevelOperatorsProxy
+module private IntelliFactory.WebSharper.Array2DModuleProxy
 
-[<Inline "null">]
-let DefaultAsyncBuilder : Control.AsyncBuilder =
-    As (AsyncBuilderProxy())
+module F = IntelliFactory.WebSharper.IntrinsicFunctionProxy
 
 [<JavaScript>]
-[<Name "array2D">]
-let CreateArray2D (rows : seq<#seq<'T>>) =
-    let arr = rows |> Seq.map (Array.ofSeq) |> Array.ofSeq |> As<'T[,]>
+[<Inline>]
+[<Name "length1">]
+let Length1 (arr: 'T[,]) = F.GetArray2DLength1 arr
+
+[<Inline>]
+[<JavaScript>]
+[<Name "length2">]
+let Length2 (arr: 'T[,]) = F.GetArray2DLength2 arr
+
+[<Inline>]
+[<JavaScript>]
+[<Name "zeroCreate">]
+let ZeroCreate (n:int) (m:int) = F.Array2DZeroCreate n m
+    
+[<Inline>]
+[<JavaScript>]
+[<Name "create">]
+let Create n m (x:'T) =
+    let arr = As<'T[,]>(Array.init n (fun _ -> Array.create m x))
     arr?dims <- 2
     arr
+     
--- IntelliFactory.WebSharper/Proxy/OperatorIntrinsics.fs
+++ IntelliFactory.WebSharper/Proxy/OperatorIntrinsics.fs
@@ -47,3 +47,65 @@ let GetArraySlice<'T> (source: 'T[]) (start: int option) (finish: int option) =
     | Some s, None -> SliceStart source s
     | None, Some f -> Slice source 0 (f + 1)
     | _ -> [||]
+
+module F = IntelliFactory.WebSharper.IntrinsicFunctionProxy
+
+[<JavaScript>]
+let SetArraySlice (dst: _[]) start finish (src:_[]) = 
+    let start  = (match start with None -> 0 | Some n -> n) 
+    let finish = (match finish with None -> dst.Length - 1 | Some n -> n) 
+    F.SetArraySub dst start (finish - start + 1) src
+
+[<JavaScript>]
+let GetArraySlice2D (arr: _[,]) start1 finish1 start2 finish2 = 
+    let start1  = (match start1 with None -> 0 | Some n -> n) 
+    let start2  = (match start2 with None -> 0 | Some n -> n) 
+    let finish1 = (match finish1 with None -> F.GetArray2DLength1 arr - 1 | Some n -> n) 
+    let finish2 = (match finish2 with None -> F.GetArray2DLength2 arr - 1 | Some n -> n) 
+    let len1 = (finish1 - start1 + 1)
+    let len2 = (finish2 - start2 + 1)
+    F.GetArray2DSub arr start1 start2 len1 len2
+
+[<JavaScript>]
+let GetArraySlice2DFixed1 (arr: _[,]) fixed1 start2 finish2 = 
+    let start2  = (match start2 with None -> 0 | Some n -> n) 
+    let finish2 = (match finish2 with None -> F.GetArray2DLength2 arr - 1 | Some n -> n) 
+    let len2 = (finish2 - start2 + 1)
+    let dst = Array.zeroCreate len2
+    for j = 0 to len2 - 1 do 
+        F.SetArray dst j (F.GetArray2D arr fixed1 (start2+j))
+    dst
+
+[<JavaScript>]
+let GetArraySlice2DFixed2 (arr: _[,]) start1 finish1 fixed2 = 
+    let start1  = (match start1 with None -> 0 | Some n -> n) 
+    let finish1 = (match finish1 with None -> F.GetArray2DLength1 arr - 1 | Some n -> n) 
+    let len1 = (finish1 - start1 + 1)
+    let dst = Array.zeroCreate len1
+    for i = 0 to len1 - 1 do 
+        F.SetArray dst i (F.GetArray2D arr (start1+i) fixed2)
+    dst
+
+[<JavaScript>]
+let SetArraySlice2DFixed1 (dst: _[,]) fixed1 start2 finish2 (src:_[]) = 
+    let start2  = (match start2 with None -> 0 | Some n -> n) 
+    let finish2 = (match finish2 with None -> F.GetArray2DLength2 dst - 1 | Some n -> n) 
+    let len2 = (finish2 - start2 + 1)
+    for j = 0 to len2 - 1 do
+        F.SetArray2D dst fixed1 (start2+j) (F.GetArray src j)
+
+[<JavaScript>]
+let SetArraySlice2DFixed2 (dst: _[,]) start1 finish1 fixed2 (src:_[]) = 
+    let start1  = (match start1 with None -> 0 | Some n -> n) 
+    let finish1 = (match finish1 with None -> F.GetArray2DLength1 dst - 1 | Some n -> n) 
+    let len1 = (finish1 - start1 + 1)
+    for i = 0 to len1 - 1 do
+        F.SetArray2D dst (start1+i) fixed2 (F.GetArray src i)
+
+[<JavaScript>]
+let SetArraySlice2D (dst: _[,]) start1 finish1 start2 finish2 (src:_[,]) = 
+    let start1  = (match start1 with None -> 0 | Some n -> n) 
+    let start2  = (match start2 with None -> 0 | Some n -> n) 
+    let finish1 = (match finish1 with None -> F.GetArray2DLength1 dst - 1 | Some n -> n) 
+    let finish2 = (match finish2 with None -> F.GetArray2DLength2 dst - 1 | Some n -> n) 
+    F.SetArray2DSub dst start1 start2 (finish1 - start1 + 1) (finish2 - start2 + 1) src
