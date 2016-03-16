5c884e97fd3dba1102c10a85b171f672d0b3f637
István Gansperger <gansperger@gmail.com>
2016-03-16 13:18:18 +0100

    Make build cmd do a clean build

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
 setlocal
 set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\14.0\Bin;%ProgramFiles(x86)%\MSBuild\12.0\Bin;%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+rd /s /q build
 MSBuild.exe msbuild\WebSharper.proj /verbosity:minimal /p:Arguments="%*"
