a2a5d6447e679b74b0eee340e65b167da7991ae2
Loic Denuziere <loic.denuziere@intellifactory.com>
2016-09-01 14:46:42 +0200

    Fix metadata loading for RPCs when outside ASP.NET

--- src/sitelets/WebSharper.Web/Shared.fs
+++ src/sitelets/WebSharper.Web/Shared.fs
@@ -20,6 +20,7 @@
 
 module WebSharper.Web.Shared
 
+open System.Collections.Generic
 module J = WebSharper.Core.Json
 module M = WebSharper.Core.Metadata
 module R = WebSharper.Core.Reflection
@@ -28,12 +29,33 @@ let private trace =
     System.Diagnostics.TraceSource("WebSharper",
         System.Diagnostics.SourceLevels.All)
 
+let private allReferencedAssemblies() =
+    let d = Dictionary<string, System.Reflection.Assembly>()
+    let rec loop (asm: System.Reflection.Assembly) =
+        asm.GetReferencedAssemblies()
+        |> Array.iter (fun asmName ->
+            if not (d.ContainsKey asmName.Name) then
+                try
+                    let asm = System.AppDomain.CurrentDomain.Load(asmName)
+                    if not asm.IsDynamic then
+                        d.Add(asmName.Name, asm)
+                        loop asm
+                with _ ->
+                    trace.TraceEvent(System.Diagnostics.TraceEventType.Warning, 1,
+                        "Failed to load referenced assembly for metadata: ", asmName.FullName))
+    let asms =
+        System.AppDomain.CurrentDomain.GetAssemblies()
+        |> Array.filter (fun asm -> not asm.IsDynamic)
+    asms |> Array.iter (fun asm -> d.Add(asm.GetName().Name, asm))
+    asms |> Array.iter loop
+    d
+    |> Seq.map (fun (KeyValue(_, v)) -> v)
+    |> List.ofSeq
+
 let private loadMetadata () =
     let before = System.DateTime.UtcNow
     let value =
-        System.Web.Compilation.BuildManager.GetReferencedAssemblies()
-        |> Seq.cast<System.Reflection.Assembly>
-        |> Seq.toList
+        allReferencedAssemblies()
         |> List.choose M.AssemblyInfo.LoadReflected
         |> M.Info.Create
     let after = System.DateTime.UtcNow
