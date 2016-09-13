82c757c35ddb0c0b5de12753603324366554acec
András Jankó <andras.janko@gmail.com>
2016-09-13 18:22:50 +0200

    4.0-beta3

--- src/compiler/WebSharper.Core/Metadata.fs
+++ src/compiler/WebSharper.Core/Metadata.fs
@@ -350,7 +350,7 @@ module IO =
         with B.NoEncodingException t ->
             failwithf "Failed to create binary encoder for type %s" t.FullName
 
-    let CurrentVersion = "4.0-beta2"
+    let CurrentVersion = "4.0-beta3"
 
     let Decode (stream: System.IO.Stream) = MetadataEncoding.Decode(stream, CurrentVersion) :?> Info   
     let Encode stream (comp: Info) = MetadataEncoding.Encode(stream, comp, CurrentVersion)
