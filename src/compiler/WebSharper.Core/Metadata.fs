f9c790a13ab49912a6056ad984d9d61296455ed2
Andras Janko <andras.janko@gmail.com>
2016-12-07 15:55:19 +0100

    Zafir 4.0-beta5

--- src/compiler/WebSharper.Core/Metadata.fs
+++ src/compiler/WebSharper.Core/Metadata.fs
@@ -367,7 +367,7 @@ module IO =
         with B.NoEncodingException t ->
             failwithf "Failed to create binary encoder for type %s" t.FullName
 
-    let CurrentVersion = "4.0-beta4"
+    let CurrentVersion = "4.0-beta5"
 
     let Decode (stream: System.IO.Stream) = MetadataEncoding.Decode(stream, CurrentVersion) :?> Info   
     let Encode stream (comp: Info) = MetadataEncoding.Encode(stream, comp, CurrentVersion)
