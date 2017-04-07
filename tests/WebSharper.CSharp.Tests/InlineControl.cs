1a59854e18f3e5c44f4a1f1780c8c34ee70804e5
Jand42 <andras.janko@gmail.com>
2017-04-07 11:26:45 +0200

    Test for C# InlineControl

---
+++ tests/WebSharper.CSharp.Tests/InlineControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSharper.Web;
+
+namespace WebSharper.CSharp.Tests
+{
+    public class InlineControlTest
+    {
+        public static Web.Control RunTestsControl =>
+            new InlineControl(() => Tests.RunTests());
+    }
+}
