1f8075700c6fe85e9ce67ec5a1c08e4c460dfcde
Jand42 <andras.janko@gmail.com>
2017-09-14 12:16:20 +0200

    C# struct remoting test

--- tests/WebSharper.CSharp.Tests/Remoting.cs
+++ tests/WebSharper.CSharp.Tests/Remoting.cs
@@ -11,12 +11,18 @@ using static WebSharper.JavaScript.Pervasives;
 
 namespace WebSharper.CSharp.Tests
 {
-    //[JavaScript]
-    //public struct TestStruct
-    //{
-    //    public int x : int;
-    //    public int y : int;
-    //}
+    [JavaScript, Serializable]
+    public struct TestStruct
+    {
+        public int X;
+        public int Y;
+
+        public TestStruct (int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
 
     [JavaScript, Serializable]
     public class TestClass
@@ -90,6 +96,14 @@ namespace WebSharper.CSharp.Tests
 
         [Remote]
         public static Task<TestClass> IncrementXY(TestClass o)
+        {
+            o.X++;
+            o.Y++;
+            return Task.FromResult(o);
+        }
+
+        [Remote]
+        public static Task<TestStruct> IncrementXYStruct(TestStruct o)
         {
             o.X++;
             o.Y++;
@@ -199,6 +213,17 @@ namespace WebSharper.CSharp.Tests
             o.X = 1;
             o.Y = 1;
             o = await Server.IncrementXY(o);
+            Equal(o.X, 2);
+            Equal(o.Y, 2);
+        }
+
+        [Test]
+        public async Task CustomStruct()
+        {
+            var o = new TestStruct();
+            o.X = 1;
+            o.Y = 1;
+            o = await Server.IncrementXYStruct(o);
             Equal(o.X, 2);
             Equal(o.Y, 2);
         }
