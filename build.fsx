ee1e5e7d01f49b7f95ab96f70f30fb7cf67f0eb6
Jand42 <andras.janko@gmail.com>
2017-11-06 16:32:29 +0100

    4.1-alpha, no nuget dep on TypeScript.MSBuild

--- build.fsx
+++ build.fsx
@@ -5,8 +5,8 @@ open System.IO
 open Fake
 open WebSharper.Fake
 
-let version = "4.0"
-let pre = None
+let version = "4.1"
+let pre = Some "alpha"
 
 let baseVersion =
     version + match pre with None -> "" | Some x -> "-" + x
