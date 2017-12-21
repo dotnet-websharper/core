036dd48020d8cab8abc64b009a677c4530de248c
András Jankó <andras.janko@gmail.com>
2017-12-22 00:54:22 +0100

    Activator.Activate checks for jQuery availability too

--- src/stdlib/WebSharper.Main/Html.fs
+++ src/stdlib/WebSharper.Main/Html.fs
@@ -71,12 +71,12 @@ module Activator =
     [<Literal>]
     let META_ID = "websharper-data"
 
-    [<Direct "typeof document !== 'undefined'">]
-    let private hasDocument () = false
+    [<Direct "typeof document !== 'undefined' && typeof jQuery !== 'undefined'">]
+    let private hasDocumentAndJQuery () = false
 
     [<JavaScript>]
     let private Activate() =
-        if hasDocument () then
+        if hasDocumentAndJQuery () then
             let meta = JS.Document.GetElementById(META_ID)
             if (As meta) then
                 JQuery.Of(JS.Document).Ready(fun () ->
