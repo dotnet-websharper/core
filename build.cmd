db000e15c846ede1f12f1ca936f97cf0de170582
Loic Denuziere <loic@denuziere.net>
2018-06-20 14:49:24 +0200

    Restore build script even if WsNoUpdate is set

--- build.cmd
+++ build.cmd
@@ -4,6 +4,9 @@ setlocal
 if "%WsNoUpdate%"=="" (
   .paket\paket.exe update -g wsbuild
   if errorlevel 1 exit /b %errorlevel%
+) else (
+  .paket\paket.exe restore -g wsbuild
+  if errorlevel 1 exit /b %errorlevel%
 )
 
 set DOTNETSOLUTION="WebSharper.sln"
