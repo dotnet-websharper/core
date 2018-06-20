4b77a08d9ee896ed458b8407be9e4ea7913e21be
Loic Denuziere <loic@denuziere.net>
2018-06-20 14:49:59 +0200

    ReflectionTypeLoadException on unpack prints the failing types

--- src/compiler/WebSharper.Compiler/commands/UnpackCommand.fs
+++ src/compiler/WebSharper.Compiler/commands/UnpackCommand.fs
@@ -139,18 +139,27 @@ module UnpackCommand =
             for r in a.GetContents() do
                 writeBinary content r.FileName (r.GetContentData())
 
-            let rec printError (e: exn) =
-                if isNull e.InnerException then
-                    e.Message
-                else e.Message + " - " + printError e.InnerException 
-            
+            let printError (e: exn) =
+                let rec messages (e: exn) =
+                    seq {
+                        yield e.Message
+                        match e with
+                        | :? System.Reflection.ReflectionTypeLoadException as e ->
+                            yield! Seq.collect messages e.LoaderExceptions
+                        | e when isNull e.InnerException -> ()
+                        | e -> yield! messages e.InnerException
+                    }
+                String.concat " - " (messages e)
+
             if cmd.DownloadResources then
                 try
                     let asm = 
                         try
                             System.Reflection.Assembly.Load (Path.GetFileNameWithoutExtension p)
                         with e ->
-                            errors.Add <| sprintf "Failed to load assembly for unpacking local resources: %s - %s" p (printError e)     
+                            if e.HResult <> 0x80131058 then
+                                errors.Add <| sprintf "Failed to load assembly for unpacking local resources: %s - %s" p (printError e)     
+                            // else this is a reference assembly, so it's ok not to load it.
                             null
                     if not (isNull asm) then
                         for t in asm.GetTypes() do
@@ -161,7 +170,7 @@ module UnpackCommand =
                                 with e ->
                                     errors.Add <| sprintf "Failed to unpack local resource: %s - %s" t.FullName (printError e)     
                 with e ->
-                    errors.Add <| sprintf "Failed to unpack local resources: %s" (printError e)     
+                    errors.Add <| sprintf "Failed to unpack local resources: %s" (printError e)
 
         if errors.Count = 0 then
             C.Ok
