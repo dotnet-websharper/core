cfb25ebe3dad6ff1767672dc6064def4eed044ac
András Jankó <andras.janko@gmail.com>
2018-07-05 15:37:37 +0200

    fix qunit link to https

--- src/stdlib/WebSharper.Testing/Resources.fs
+++ src/stdlib/WebSharper.Testing/Resources.fs
@@ -5,7 +5,7 @@ open WebSharper
 [<Require(typeof<JQuery.Resources.JQuery>)>]
 [<Sealed>]
 type QUnit() =
-    inherit Resources.BaseResource("//code.jquery.com/qunit/",
+    inherit Resources.BaseResource("https://code.jquery.com/qunit/",
         "qunit-1.18.0.js", "qunit-1.18.0.css")
 
 [<assembly: Require(typeof<QUnit>)>]
