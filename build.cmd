7b7ff4ee8ca18869a03f6ea737ab59ab3f15c4fd
András Jankó <andras.janko@gmail.com>
2020-01-29 21:31:16 +0100

    fix restore by setting DOTNETSOLUTION

--- build.cmd
+++ build.cmd
@@ -5,5 +5,6 @@ dotnet restore WebSharper.Compiler.sln
 dotnet restore WebSharper.sln
 if errorlevel 1 exit /b %errorlevel%
 
+set DOTNETSOLUTION="WebSharper.Compiler.sln;WebSharper.sln"
 call paket-files\wsbuild\github.com\dotnet-websharper\build-script\update.cmd
 call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
