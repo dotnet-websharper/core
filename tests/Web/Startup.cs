5bdc15a9c4cab0ce16f87c99ac10e39d51667a1f
András Jankó <andras.janko@gmail.com>
2020-11-26 11:17:44 +0100

    add new Web test project

---
+++ tests/Web/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSharper.AspNetCore;
+
+namespace Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSitelet()
+            //    .AddAuthentication("WebSharper")
+            //    .AddCookie("WebSharper", options => { });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+            app.UseAuthentication()
+                .UseStaticFiles()
+                .UseWebSharper()
+                .Run(context =>
+                {
+                    context.Response.StatusCode = 404;
+                    return context.Response.WriteAsync("Page not found");
+                });
+        }
+
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
