8765fd350cee9be987cd2c66a34f27ca189358da
András Jankó <andras.janko@gmail.com>
2021-07-01 10:27:51 +0200

    simplify build script

--- build.cmd
+++ build.cmd
@@ -2,13 +2,9 @@
 setlocal
 
 dotnet tool restore
+
 dotnet paket update -g wsbuild --no-install
-
-call paket-files\wsbuild\github.com\dotnet-websharper\build-script\update.cmd
-
-dotnet paket restore
-dotnet restore WebSharper.Compiler.sln
-dotnet restore WebSharper.sln
+if errorlevel 1 exit /b %errorlevel%
 
 set DOTNETSOLUTION="WebSharper.Compiler.sln;WebSharper.sln"
 call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 dotnet tool restore
+
 dotnet paket update -g wsbuild --no-install
-
-. paket-files/wsbuild/github.com/dotnet-websharper/build-script/update.sh
-
-dotnet paket restore
-dotnet restore WebSharper.Compiler.sln
-dotnet restore WebSharper.sln
 
 export DOTNETSOLUTION="WebSharper.Compiler.sln;WebSharper.sln"
 . paket-files/wsbuild/github.com/dotnet-websharper/build-script/build.sh "$@"
