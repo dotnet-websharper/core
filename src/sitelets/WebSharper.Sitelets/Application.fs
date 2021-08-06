c10a2adcba3e098f91089eb721f6efb6737034eb
btamas2000 <83249036+btamas2000@users.noreply.github.com>
2021-08-06 09:42:28 +0200

    Fixed Application.Text overload issue (#1178)

--- src/sitelets/WebSharper.Sitelets/Application.fs
+++ src/sitelets/WebSharper.Sitelets/Application.fs
@@ -50,8 +50,8 @@ type Application =
                 }
         }
 
-    static member Text (t: Context<SPA.EndPoint> -> string) : Sitelet<SPA.EndPoint> =
-        Application.SinglePage (fun context -> Content.Text (t context))
+    // static member Text (t: Context<SPA.EndPoint> -> string) : Sitelet<SPA.EndPoint> =
+    //     Application.SinglePage (fun context -> Content.Text (t context))
 
     static member SinglePage (f: Func<Context<SPA.EndPoint>, Task<Content<SPA.EndPoint>>>) : Sitelet<SPA.EndPoint> =
         Application.SinglePage (fun ctx -> f.Invoke ctx |> Async.AwaitTask)
