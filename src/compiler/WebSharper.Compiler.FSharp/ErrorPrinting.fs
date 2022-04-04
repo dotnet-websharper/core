5ae426d7b1ed81a7b8aa6845a51301ae5321fbf1
András Jankó <andras.janko@gmail.com>
2022-04-04 21:33:18 +0200

    #1218 use WebSharperCompile task again

--- src/compiler/WebSharper.Compiler.FSharp/ErrorPrinting.fs
+++ src/compiler/WebSharper.Compiler.FSharp/ErrorPrinting.fs
@@ -43,7 +43,7 @@ type WarnSettings =
             WarnAsError = Set []
             AllWarnAsError = false
             DontWarnAsError = Set []
-            VSStyleErrors = true
+            VSStyleErrors = false
         }   
         
     member this.CheckNoWarn(c) =
@@ -54,7 +54,7 @@ type WarnSettings =
         || (this.AllWarnAsError && this.DontWarnAsError.Contains(c))
 
 let PrintGlobalError (logger: LoggerBase) err =
-    sprintf "output error FS9001: %s" (NormalizeErrorString err)
+    sprintf "WebSharper error WS9001: %s" (NormalizeErrorString err)
     |> logger.Error
 
 let PrintGlobalWarning (warnSettings: WarnSettings) (logger: LoggerBase) err =
@@ -63,7 +63,7 @@ let PrintGlobalWarning (warnSettings: WarnSettings) (logger: LoggerBase) err =
     elif warnSettings.CheckWarnAsError(9002) then
         PrintGlobalError logger err
     else
-        sprintf "output warning FS9002: %s" (NormalizeErrorString err)
+        sprintf "WebSharper warning WS9002: %s" (NormalizeErrorString err)
         |> logger.Error
 
 // see https://github.com/fsharp/FSharp.Compiler.Service/blob/533e728f08f4f9f8527b58877d377f9d6eed09ce/src/fsharp/CompileOps.fs#L380
@@ -120,12 +120,12 @@ let PrintWebSharperErrors warnOnly (projFile: string) (warnSettings: WarnSetting
             ()     
         else
             let subcategory =
-                if warnSettings.VSStyleErrors then "output " else ""        
+                if warnSettings.VSStyleErrors then "WebSharper " else ""        
             let severity =
                 if (isError && not warnOnly) || (not isError && warnSettings.CheckWarnAsError(9002)) then 
-                    "error FS9001" 
+                    "error WS9001" 
                 else 
-                    "warning FS9002"
+                    "warning WS9002"
             match pos with
             | Some pos ->
                 sprintf "%s(%d,%d,%d,%d): %s%s: %s" 
