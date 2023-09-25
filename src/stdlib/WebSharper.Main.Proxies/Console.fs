388a579f41e80b7b3e573f998967d54cce4f3a68
GyGerg <gyalus.gergo.98@gmail.com>
2023-09-25 11:45:41 +0200

    #775 Add proxy for stderr / System.Console.Error

--- src/stdlib/WebSharper.Main.Proxies/Console.fs
+++ src/stdlib/WebSharper.Main.Proxies/Console.fs
@@ -19,12 +19,160 @@
 // $end{copyright}
 
 module internal WebSharper.Console
+open WebSharper.JavaScript
 
 type C = JavaScript.Console
 
+[<Proxy(typeof<System.IO.TextWriter>)>]
+type TextWriterProxy =
+    new() = {}
+    
+    abstract Encoding : System.Text.Encoding
+    default _.Encoding
+        with get():System.Text.Encoding = null
+
+    abstract Write : bool -> unit
+    default _.Write(x: bool) = C.Log(x)
+    abstract Write : char -> unit
+    default _.Write(x: char) = C.Log(string x)
+    abstract Write : char[]*int*int -> unit
+    default _.Write(x: char[], s:int, c:int) = C.Log(System.String(Array.sub x s c)) // todo
+    abstract Write : string*obj[] -> unit
+    default _.Write(x: string, o: obj[]) = C.Log(System.String.Format(x,o))
+    abstract Write : string * obj -> unit
+    default _.Write(x: string, o: obj) = C.Log(System.String.Format(x,o))
+    abstract Write : uint64 -> unit
+    default _.Write(x: uint64) = C.Log(string x)
+    abstract Write : uint32 -> unit
+    default _.Write(x: uint32) = C.Log(string x)
+    abstract Write : string -> unit
+    default _.Write(x: string) = C.Log(x)
+    abstract Write : float -> unit
+    default _.Write(x: float) = C.Log(x)
+    abstract Write : single -> unit
+    default _.Write(x: single) = C.Log(x)
+    abstract Write : string * obj * obj -> unit
+    default _.Write(x: string, o1: obj, o2: obj) = C.Log(x)
+    abstract Write : obj -> unit
+    default _.Write(o: obj) = C.Log(string o)
+    abstract Write : int64 -> unit
+    default _.Write(x: int64) = C.Log(string x)
+    abstract Write : int32 -> unit
+    default _.Write(x: int32) = C.Log(string x)
+    abstract Write : char[] -> unit
+    default _.Write(x: char[]) = C.Log(System.String(x))
+    
+    abstract WriteLine : unit -> unit
+    default _.WriteLine() = C.Log("")
+    
+    abstract WriteLine : bool -> unit
+    default _.WriteLine(x: bool) = C.Log(x)
+    abstract WriteLine : char -> unit
+    default _.WriteLine(x: char) = C.Log(string x)
+    abstract WriteLine : char[]*int*int -> unit
+    default _.WriteLine(x: char[], s:int, c:int) = C.Log(System.String(Array.sub x s c)) // todo
+    abstract WriteLine : string*obj[] -> unit
+    default _.WriteLine(x: string, o: obj[]) = C.Log(System.String.Format(x,o))
+    abstract WriteLine : string * obj -> unit
+    default _.WriteLine(x: string, o: obj) = C.Log(System.String.Format(x,o))
+    abstract WriteLine : uint64 -> unit
+    default _.WriteLine(x: uint64) = C.Log(string x)
+    abstract WriteLine : uint32 -> unit
+    default _.WriteLine(x: uint32) = C.Log(string x)
+    abstract WriteLine : string -> unit
+    default _.WriteLine(x: string) = C.Log(x)
+    abstract WriteLine : float -> unit
+    default _.WriteLine(x: float) = C.Log(x)
+    abstract WriteLine : single -> unit
+    default _.WriteLine(x: single) = C.Log(x)
+    abstract WriteLine : string * obj * obj -> unit
+    default _.WriteLine(x: string, o1: obj, o2: obj) = C.Log(x)
+    abstract WriteLine : string * obj * obj * obj -> unit
+    default _.WriteLine(x: string, o1: obj, o2: obj, o3: obj) = C.Log(x)
+    abstract WriteLine : obj -> unit
+    default _.WriteLine(o: obj) = C.Log(string o)
+    abstract WriteLine : int64 -> unit
+    default _.WriteLine(x: int64) = C.Log(string x)
+    abstract WriteLine : int32 -> unit
+    default _.WriteLine(x: int32) = C.Log(string x)
+    abstract WriteLine : char[] -> unit
+    default _.WriteLine(x: char[]) = C.Log(System.String(x))
+
+
+[<JavaScript>]
+type OutTextWriter() =
+    inherit System.IO.TextWriter()
+
+    override _.Encoding
+        with get() : System.Text.Encoding = null
+
+
+[<JavaScript>]
+type ErrorTextWriter() =
+    inherit System.IO.TextWriter()
+
+    override _.Encoding
+        with get() : System.Text.Encoding = null
+
+    override _.Write(x: bool) = C.Error(x)
+    override _.Write(x: char) = C.Error(string x)
+    override _.Write(x: char[], s:int, c:int) = C.Error(System.String(Array.sub x s c)) // todo
+    override _.Write(x: string, o: obj[]) = C.Error(System.String.Format(x,o))
+    override _.Write(x: string, o: obj) = C.Error(System.String.Format(x,o))
+    override _.Write(x: uint64) = C.Error(string x)
+    override _.Write(x: uint32) = C.Error(string x)
+    override _.Write(x: string) = C.Error(x)
+    override _.Write(x: float) = C.Error(x)
+    override _.Write(x: single) = C.Error(x)
+    override _.Write(x: string, o1: obj, o2: obj) = C.Error(x)
+    override _.Write(o: obj) = C.Error(string o)
+    override _.Write(x: int64) = C.Error(string x)
+    override _.Write(x: int32) = C.Error(string x)
+    override _.Write(x: char[]) = C.Error(System.String(x))
+    override _.WriteLine() = C.Error("")
+        
+    override _.WriteLine(x: bool) = C.Error(x)
+        
+    override _.WriteLine(x: char) = C.Error(string x)
+        
+    override _.WriteLine(x: char[]) = C.Error(System.String(x))
+        
+    override _.WriteLine(x: char[], s: int, c: int) = C.Error(System.String(Array.sub x s c))
+        
+    override _.WriteLine(x: float) = C.Error(x)
+        
+    override _.WriteLine(x: int) = C.Error(x)
+        
+    override _.WriteLine(x: int64) = C.Error(x)
+        
+    override _.WriteLine(x: obj) = C.Error(string x)
+        
+    override _.WriteLine(x: single) = C.Error(x)
+        
+    override _.WriteLine(x: string) = C.Error(x)
+        
+    override _.WriteLine(x: string, o1: obj) = C.Error(System.String.Format(x, o1))
+        
+    override _.WriteLine(x: string, o1: obj, o2: obj) = C.Error(System.String.Format(x, o1, o2))
+        
+    override _.WriteLine(x: string, o1: obj, o2: obj, o3: obj) = C.Error(System.String.Format(x, o1, o2, o3))
+        
+    override _.WriteLine(x: string, o: obj[]) =  C.Error(System.String.Format(x, o))
+        
+    override _.WriteLine(x: uint32) = C.Error(x)
+        
+    override _.WriteLine(x: uint64) = C.Error(x)
+
+let [<JavaScript>] errorWriter = new ErrorTextWriter()
+let [<JavaScript>] outWriter = new OutTextWriter()
 [<Proxy(typeof<System.Console>)>]
 type ConsoleProxy =
+    
+    static member Error 
+        with [<Inline;JavaScript>] get() : System.IO.TextWriter = errorWriter
 
+    static member Out 
+        with [<Inline;JavaScript>] get() : System.IO.TextWriter = outWriter
     [<Inline>]
     static member WriteLine() = C.Log("")
     [<Inline>]
