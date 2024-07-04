ad4b09077dc4857f587285f01b717c3f40968b06
Jooseppi12 <urijozsef@europe.com>
2024-07-04 13:47:52 +0200

    WIP

--- build.cmd
+++ build.cmd
@@ -7,4 +7,4 @@ dotnet paket update -g wsbuild --no-install
 if errorlevel 1 exit /b %errorlevel%
 
 set DOTNETSOLUTION="WebSharper.Compiler.sln;WebSharper.sln"
-call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
+dotnet fsi ./build.fsx -t %*
