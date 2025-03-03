39f9ec6a1c137bc8223c4a2a72516ee6f212cd1a
András Jankó <andras.janko@gmail.com>
2025-03-03 13:30:31 +0100

    #1438 C# offline sitelets test

---
+++ tests/WebSharper.CSharp.StaticHtml.Tests/esbuild.config.mjs
@@ -0,0 +1,23 @@
+﻿import { readdirSync, cpSync } from 'fs'
+import { build } from 'esbuild'
+
+cpSync('./build/', './bin/html/', { recursive: true });
+
+const files = readdirSync('./build/Scripts/WebSharper/WebSharper.CSharp.StaticHtml.Tests/');
+
+files.forEach(file => {
+  if (file.endsWith('.js')) {
+    var options =
+    {
+      entryPoints: ['./build/Scripts/WebSharper/WebSharper.CSharp.StaticHtml.Tests/' + file],
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      outfile: 'bin/html/Scripts/WebSharper/' + file,
+      globalName: 'wsbundle'
+    };
+
+    console.log("Bundling:", file);
+    build(options);
+  }
+});
