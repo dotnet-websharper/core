2d669a7ba37817726094968865f010dc2ed56290
András Jankó <andras.janko@gmail.com>
2025-07-11 15:48:20 +0200

    script changes and PackageIds

--- build.sh
+++ build.sh
@@ -5,4 +5,8 @@ dotnet tool restore
 dotnet paket update -g wsbuild --no-install
 
 export DOTNETSOLUTION="WebSharper.Compiler.sln;WebSharper.sln"
-. paket-files/wsbuild/github.com/dotnet-websharper/build-script/build.sh "$@"
+
+# Allow running `build SomeTask` instead of `build -t SomeTask`
+if [ "$1" != "" -a "$1" != -t -a "$1" != --target ]; then EXTRA_ARG=-t; else EXTRA_ARG=; fi
+
+dotnet fsi build.fsx $EXTRA_ARG "$@"
